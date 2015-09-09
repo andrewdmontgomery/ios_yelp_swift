ef91922a8cd6d54d2ecc3888bba845db5928edea
Andrew Montgomery <andrewdmontgomery@mac.com>
2015-09-08 22:28:55 -0400

    Searching functional

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,16 +8,25 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, FiltersViewControllerDelegate {
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, FiltersViewControllerDelegate, UISearchBarDelegate {
 
     var businesses: [Business]!
+    
+//    var searchTerm = ""
+    
+    var categories: [String]?
     
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
+                
+        let searchBar = UISearchBar()
+        searchBar.sizeToFit()
+        searchBar.delegate = self
+        navigationItem.titleView = searchBar
+        
+        Business.searchWithTerm(searchBar.text, completion: { (businesses: [Business]!, error: NSError!) -> Void in
             self.businesses = businesses
             self.tableView.reloadData()
             
@@ -49,6 +58,21 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
         // Dispose of any resources that can be recreated.
     }
 
+//    func performSearchWithTerm(searchTerm: String, sort: YelpSortMode?, categories: [String]?, deals: Bool) {
+//                Business.searchWithTerm(searchTerm, sort: .Distance, categories: categories, deals: nil) { (businesses: [Business]!, error: NSError!) -> Void in
+//                    self.businesses = businesses
+//                    self.tableView.reloadData()
+//        
+//                    for business in businesses {
+//                        println(business.name!)
+//                        println(business.address!)
+//                    }
+//                }
+//
+//    }
+    
+    // MARK: - UITableView
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if businesses != nil {
             return businesses!.count
@@ -76,11 +100,61 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
     }
 
     func filtersViewController(filtersViewController: FiltersViewController, didUpdateFilters filters: [String : AnyObject]) {
-        var categories = filters["categories"] as? [String]
+        categories = filters["categories"] as? [String]
         
-        Business.searchWithTerm("Restaurants", sort: nil, categories: categories, deals: nil) { (businesses: [Business]!, error: NSError!) -> Void in
+        var searchText = ""
+        
+        if let searchBar = navigationItem.titleView as? UISearchBar {
+            searchText = searchBar.text
+        }
+        
+        Business.searchWithTerm(searchText, sort: nil, categories: categories, deals: nil) { (businesses: [Business]!, error: NSError!) -> Void in
             self.businesses = businesses
             self.tableView.reloadData()
         }
+    }
+    
+    // MARK: - UISearchControllerDelegate
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        println("searchBarTextDidChange")
+        // Only perform a search when the (x) is pressed.
+        // Otherwise, wait for searchBarSearchButtonClicked(seachBar: UISearchBar) to be called manually
+        if count(searchText) == 0 {
+            Business.searchWithTerm(searchText, sort: nil, categories: categories, deals: nil) { (businesses: [Business]!, error: NSError!) -> Void in
+                self.businesses = businesses
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        println("searchBarSearchButtonClicked")
+        let searchText = searchBar.text
+        searchBar.endEditing(true)
+        Business.searchWithTerm(searchText, sort: nil, categories: categories, deals: nil) { (businesses: [Business]!, error: NSError!) -> Void in
+            self.businesses = businesses
+            self.tableView.reloadData()
+        }
+    }
+
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        let searchText = searchBar.text
+        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            searchBar.endEditing(true)
+        })
+        Business.searchWithTerm(searchText, sort: nil, categories: categories, deals: nil) { (businesses: [Business]!, error: NSError!) -> Void in
+            self.businesses = businesses
+            self.tableView.reloadData()
+        }
+    }
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        searchBar.showsCancelButton = true
+    }
+    
+    func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
     }
 }
